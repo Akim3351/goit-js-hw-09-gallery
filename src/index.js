0bd36edcb4739456c4f0fd25a323ae70472590b9
Akim Grakovskiy <aakim5586@gmail.com>
2021-06-22 23:54:30 +0300

    22.06.2021 23-55

--- src/index.js
+++ src/index.js
@@ -1,1 +1,60 @@
 import './sass/main.scss';
+import './js/gallery.js';
+
+const galleryMarkup = galleryItems.map(({ preview, original, description }, index) =>
+`<li class = "gallery__item">
+<a class = "gallery__link" href = " ">
+<img class = "gallery__image" src = "${preview}" data-source = "${original}"
+alt = "${description}" data-index = "${index}">
+</a>
+</li>
+`).join('');
+
+const boxElems = {
+  modal: document.querySelector('.lightbox'),
+  jsGallery: document.querySelector('.js-gallery'),
+  modalImage: document.querySelector('.lightbox__image'),
+  modalBtn: document.querySelector('.lightbox__button'),
+
+};
+
+
+console.log(galleryMarkup);
+
+boxElems.jsGallery.innerHTML = galleryMarkup;
+
+const openModalClick = element => {
+
+element.preventDefault();
+
+if (element.target.localName === 'img') {
+    boxElems.modalImage.src = element.target.dataset.source;
+    boxElems.modalImage.alt = element.target.alt;
+    boxElems.modalImage.dataset.index = element.target.dataset.index;
+    boxElems.modal.classList.add('is-open');
+    window.addEventListener('keydown', escClick);
+    window.addEventListener('click', closeModalClick);
+    
+}
+};
+
+const closeWindow = () => {
+boxElems.modal.classList.remove('is-open')
+boxElems.modalImage.src = '';
+boxElems.modalImage.alt = '';
+};
+
+const escClick = element => {  
+if (element.key === 'Escape') {
+  closeWindow();
+}
+};
+
+const closeModalClick = element  => {
+if (element.target.localName !== 'img') {
+  closeWindow();
+
+}
+};
+
+boxElems.jsGallery.addEventListener('click', openModalClick);
